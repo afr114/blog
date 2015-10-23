af663e4101ba0fe11b3bfd131300e682f08cdb5a
Abdul Rahimi <afr114@gmail.com>
2015-10-23 16:55:40 -0700

    add devise login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admins
-  root :to => 'users#index'
+    root :to => 'users#index'
     resources :users, :only => [:index, :show]
       namespace :admin do
           get 'dashboard' => 'posts#index'
         resources :posts do
-          resources :tags 
+          resources :tags
       end
     end
 end
